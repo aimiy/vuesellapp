e7fe6f72d8c12c7d484923b33bcf7dc4ef581810
aimiy <mioly@foxmail.com>
2017-07-05 14:14:57 +0800

    ready

--- sell/src/App.vue
+++ sell/src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
     <v-header></v-header>
-
     <div class="tab">
       <div class="tab-item">商品</div>
       <div class="tab-item">评价</div>
@@ -15,7 +14,6 @@
 
 <script>
 import header from './components/header/header.vue';
-
 export default{
     components: {
         'v-header': header
@@ -23,6 +21,14 @@ export default{
 };
 </script>
 
-<style>
-
+<style lang="stylus" rel="stylesheet/stylus">
+  #app
+    .tab
+      display :flex
+      width:100%
+      height :40px
+      line-height: 40px
+      .tab-item
+        flex:1
+        text-align :center
 </style>
--- sell/src/components/header/header.vue
+++ sell/src/components/header/header.vue
@@ -2,11 +2,9 @@
   <div class="header">
     我是header
   </div>
-
 </template>
 <script type="text/ecmascript-6">
 export default{};
-
 </script>
 <style lang="stylus" rel="stylesheet/stylus">
 
