bf600b57e7fe419060d3afe69b57298307eb677a
aimiy <mioly@foxmail.com>
2017-07-06 18:01:48 +0800

    router

--- sell/src/main.js
+++ sell/src/main.js
@@ -2,14 +2,22 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import App from './App';
-import router from './router';
+import VueRouter from 'vue-router';
+import goods from './components/goods/goods';
+import ratings from './components/ratings/ratings';
+import seller from './components/seller/seller';
 
-Vue.config.productionTip = false;
-
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
+Vue.use(VueRouter);
+const routes = [
+  {path: '/goods', component: goods},
+  {path: '/ratings', component: ratings},
+  { path: '/seller', component: seller }
+];
+const router = new VueRouter({
+  routes
 });
+new Vue({
+  template: '<App/>',
+  components: { App },
+  router
+}).$mount('#app');
