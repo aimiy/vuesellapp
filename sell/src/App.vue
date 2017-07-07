04237b43fd3ac6c67f61e0e4bb9f3b97f669f77e
aimiy <mioly@foxmail.com>
2017-07-07 17:12:16 +0800

    header

--- sell/src/App.vue
+++ sell/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <v-header></v-header>
-    <div class="tab">
+    <v-header :seller="seller"></v-header>
+    <div class="tab border-1px">
       <div class="tab-item">
         <router-link to="/goods">商品</router-link></div>
       <div class="tab-item">
@@ -14,7 +14,23 @@
 </template>
 <script>
 import header from './components/header/header.vue';
+const ERR_OK = 0;
 export default{
+    data () {
+        return {
+            seller: { }
+        };
+    },
+    created () {
+        this.$http.get('/api/seller').then(response => {
+            response = response.body;
+            console.log(response);
+            if (response.errno === ERR_OK) {
+                this.seller = response.data;
+                console.log(this.seller.avatar);
+            }
+        });
+  },
     components: {
         'v-header': header
     }
@@ -22,12 +38,20 @@ export default{
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "./common/stylus/mixin.styl"
   .tab
     display :flex
     width:100%
     height :40px
     line-height: 40px
+    border-1px(rgba(1,17,12,0.1))
     .tab-item
       flex:1
       text-align :center
+      & > a
+        display :block
+        font-size :14px
+        color: rgb(77,85,93)
+        &.active
+          color :rgb(240,20,20)
 </style>
