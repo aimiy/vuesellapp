f677cf06a17c8b03d4ef531690e51cd70877ebb8
aimiy <mioly@foxmail.com>
2017-07-10 18:09:26 +0800

    header

--- sell/src/components/header/header.vue
+++ sell/src/components/header/header.vue
@@ -13,12 +13,20 @@
           {{seller.description}}/{{seller.deliveryTime}}分钟送达
         </div>
         <div v-if="seller.supports" class="support">
-          <span class="icon"></span>
+          <span class="icon" :class="classMap[seller.supports[0].type]"></span>
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
+      <div v-if="seller.supports" class="support-count">
+        <span class="count">{{seller.supports.length}}个</span>
+        <i class="icon-keyboard_arrow_right"></i>
+      </div>
     </div>
-    <div class="bulletin-wrapper"></div>
+    <div class="bulletin-wrapper">
+      <span class="bulletin-title"></span>
+      <span class="bulletin-text">{{seller.bulletin}}</span>
+      <i class=".icon-keyboard_arrow_right"></i>
+    </div>
   </div>
 </template>
 <script type="text/ecmascript-6">
@@ -27,34 +35,103 @@ export default{
         seller: {
             type: Object
         }
-    }
+    },
+  created() {
+        this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+  }
 };
 </script>
 <style lang="stylus" rel="stylesheet/stylus">
-  @import "../../common/stylus/mixin.styl"
+  @import "../../common/stylus/mixin";
 .header
   color: #fff
-  background-color :#000000
+  background-color :#999
   .content-wrapper
+    position :relative
     padding :24px 12px 18px 24px
     font-size :0
     .avatar
       display:inline-block
+      vertical-align :top
+      img
+        border-radius :2px
     .content
       display :inline-block
       margin-left:16px
-      font-size :14px
       .title
         margin :2px 0 8px 0
         .brand
           display inline-block
+          vertical-align: top
           width :30px
           height: 18px
           bg-image('brand')
           background-size :30px 18px
           background-repeat :no-repeat
         .name
-          font-size :18px
+          margin-left 6px
+          font-size :16px
+          line-height: 18px
 
 
+      .description
+        margin-bottom :10px
+        line-height: 12px
+        font-size :12px
+      .support
+        .icon
+          display :inline-block
+          height: 12px
+          width: 12px
+          vertical-align :top
+          margin-right :4px
+          background-size :12px 12px
+          background-repeat :no-repeat
+          &.decrease
+            bg-image('decrease_1')
+          &.discount
+            bg-image('discount_1')
+          &.guarantee
+            bg-image('guarantee_1')
+          &.invoice
+            bg-image('invoice_1')
+          &.special
+            bg-image('special_1')
+        .text
+          line-height: 12px
+          font-size :10px
+    .support-count
+      position :absolute
+      right :12px
+      bottom :14px
+      padding:0 8px
+      line-height: 24px
+      border-radius :14px
+      background : rgba(0, 0, 0, 0.2)
+      text-align :center
+      .count
+        vertical-align :top
+        font-size :10px
+      .icon-keyboard_arrow_right
+        margin-left : 2px
+        line-height: 24px
+        font-size :10px
+  .bulletin-wrapper
+    height: 28px
+    line-height: 28px
+    padding :0 22px 0 12px
+    white-space :nowrap
+    overflow :hidden
+    text-overflow :ellipsis
+    font-size :0
+    .bulletin-title
+      display :inline-block
+      width: 22px
+      height: 12px
+      bg-img('bulletin')
+      background-size :22px 12 px
+      background-repeat :no-repeat
+    .bulletin-text
+      margin :0 4px
+      font-size :10px
 </style>
